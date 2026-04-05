669c35f0dd9b9cf92a5e960c74901289ea2af586
ostinsmash <ostinsmashxx@gmail.com>
2026-04-05 16:43:35 -0600

    Reportes-Austin-Commit

---
+++ DentalCare.UI/DentalCare.UI/Models/Reporteria/ReportePagosViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DentalCare.UI.Models.Reporteria
+{
+    public class ReportePagosViewModel
+    {
+        public DateTime Fecha { get; set; }
+        public string Paciente { get; set; }
+        public decimal Monto { get; set; }
+    }
+}
