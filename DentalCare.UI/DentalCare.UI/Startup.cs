7ff56bb96194815c3f7ca4dffcf9117566d6f2b2
Werner Trujillo <wernertrujillo26@gmail.com>
2026-05-28 11:07:43 -0600

    realización de login y registro

---
+++ DentalCare.UI/DentalCare.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DentalCare.UI.Startup))]
+namespace DentalCare.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ DentalCare.UI/SolucionLogin.UI.identity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SolucionLogin.UI.identity.MvcApplication" Language="C#" %>
