b02afa77dbc39b8494bed0ca7c247fcd2ad7532f
Werner Trujillo <wernertrujillo26@gmail.com>
2026-06-04 02:19:02 -0600

    commit fast

---
+++ DentalCare.UI/DentaCare.LogicaDeNegocio/Usuarios/DesactivarUsuario/DesactivarUsuarioLN.cs
@@ -0,0 +1,22 @@
+﻿using DentalCare.Abstraccion.AccesoADatos.Usuarios.DesactivarUsuario;
+using DentalCare.Abstraccion.LogicaDeNegocio.Usuarios.DesactivarUsuario;
+using DentalCare.AccesoADatos.Usuarios.DesactivarUsuario;
+
+namespace DentalCare.LogicaDeNegocio.Usuarios.DesactivarUsuario
+{
+    public class DesactivarUsuarioLN : IDesactivarUsuarioLN
+    {
+        private readonly IDesactivarUsuarioAD _desactivarAD;
+
+        public DesactivarUsuarioLN()
+        {
+            _desactivarAD = new DesactivarUsuarioAD();
+        }
+
+        public string Desactivar(int idUsuario)
+        {
+            _desactivarAD.Desactivar(idUsuario);
+            return null;
+        }
+    }
+}
